109f6ca2722dbda3032205e1b96e796908ac13ca
hayzee <hayzee@users.noreply.github.com>
2024-09-19 00:16:07 +0100

    To creating a component from within another component, a vector should be used rather than a standard function call.

--- src/flowfun/components/content/panel.cljs
+++ src/flowfun/components/content/panel.cljs
@@ -156,9 +156,6 @@
    [:div
     {:class
      "p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14"}
-    (row-3)
-    (group-4)
-    (row-1 "big-row")
-    (group-4)
-
-    ]])
+    [row-3]
+    [group-4]
+    [row-1 "big-row"]]])
--- src/flowfun/components/layout/app.cljs
+++ src/flowfun/components/layout/app.cljs
@@ -1,11 +1,11 @@
 (ns flowfun.components.layout.app
   (:require [flowfun.components.layout.nav :as nav]
             [flowfun.components.layout.sidebar :as sidebar]
-            [flowfun.components.content.panel :as content]))
+            [flowfun.components.content.panel :as panel]))
 
 (defn component
   []
   [:viewport
-   (nav/component)
-   (sidebar/component)
-   (content/component)])
+   [nav/component]
+   [sidebar/component]
+   [panel/component]])
