923107aa11c46c8f7d6c8584d4800c6c09b89de4
hayzee <hayzee@users.noreply.github.com>
2024-09-30 09:01:17 +0100

    More stuff

---
+++ src/flowfun/pages/blank.cljs
@@ -0,0 +1,13 @@
+(ns flowfun.pages.example-02
+  (:require [flowfun.components.dummy :as dummy]
+            [flowfun.layout.panel-row :as panel-row]))
+
+(defn example-02-page
+  []
+  [:div
+   {:class
+    "p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14"}
+   [panel-row/row-of-n 8 [dummy/stateful-component] [dummy/stateful-component] [dummy/stateless-component] [dummy/stateful-component] [dummy/stateful-component] [dummy/stateful-component] [dummy/stateless-component] [dummy/stateful-component]]
+   [panel-row/row-of-n 3 [dummy/stateful-component] [dummy/stateless-component] [dummy/stateful-component]]
+   [panel-row/row-of-n 2 [dummy/stateful-component] [dummy/stateful-component]]
+   [panel-row/row-of-n 1 [dummy/stateless-component]]])
