8ce8ade4e4a729c288959fbd22be173bd1648aaa
hayzee <hayzee@users.noreply.github.com>
2025-09-24 23:59:52 +0100

    Centre modal with h-screen flex on parent div

--- src/flowfun/components/modals/confirm.cljs
+++ src/flowfun/components/modals/confirm.cljs
@@ -13,18 +13,16 @@
   [:div
    {:id       element-id
     :tabindex "-1"
-    :data-modal-placement "top-right"
-    :class "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    :class "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 h-screen flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
     }
    [:div
-    {:class "relative p-4 w-full max-w-md max-h-full"}
+    {:class "p-4 w-full max-w-md max-h-full"}
     [:div
-     {:class "relative bg-white rounded-lg shadow-sm dark:bg-gray-700"}
+     {:class "relative bg-white rounded-lg shadow-sm dark:bg-gray-800"}
      [:button
       {:type            "button"
        :class
        "absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-       :data-modal-hide element-id
        :on-click f-on-close}
       [:svg
        {:class       "w-3 h-3"
@@ -57,19 +55,15 @@
         "mb-5 text-lg font-normal text-gray-500 dark:text-gray-400"}
        prompt]
       [:button
-       {:data-modal-hide element-id
-        :type            "button"
+       {:type            "button"
         :class
-        ;"text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
         "text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
         :on-click f-on-yes
         }
        yes-text]
       [:button
-       {:data-modal-hide element-id
-        :type            "button"
+       {:type            "button"
         :class
         "py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        :on-click f-on-no
-        }
+        :on-click f-on-no}
        no-text]]]]])
