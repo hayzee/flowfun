b4bcdbd067c3a08615980a59f4b64830b5aea05e
hayzee <hayzee@users.noreply.github.com>
2025-10-07 20:40:34 +0100

    Did some additional stuff.

--- src/flowfun/pages/toastie.cljs
+++ src/flowfun/pages/toastie.cljs
@@ -1,6 +1,5 @@
 (ns flowfun.pages.toastie
-  (:require [flowfun.components.dummy :as dummy]
-            [flowfun.layout.panel-row :as panel-row]
+  (:require [flowfun.layout.panel-row :as panel-row]
             [flowfun.components.toast.core :as toast]))
 
 (defn bunch-o-toasts
@@ -43,10 +42,31 @@
      [:button
       {:type "button",
        :class
-       "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+       "focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
        :on-click #(toast/create-toast "This is some purple toast, it is very purple" :color "purple")
        }
       "Purple"]
+     [:button
+      {:type "button",
+       :class
+       "focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-800 dark:focus:ring-gray-500"
+       :on-click #(toast/create-toast "This is some purple toast, it is very purple" :color "gray")
+       }
+      "Gray"]
+     [:button
+      {:type "button",
+       :class
+       "focus:outline-none text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-900"
+       :on-click #(toast/create-toast "This is some indigo toast, it is very indigo" :color "indigo")
+       }
+      "Indigo"]
+     [:button
+      {:type "button",
+       :class
+       "focus:outline-none text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-900"
+       :on-click #(toast/create-toast "This is some pink toast, it is very pink" :color "pink")
+       }
+      "Pink"]
      ]
     ]])
 
